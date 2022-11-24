c2a7e6be7c801f1deb63baf0cc3625367b753ebf
Vitoria-m <vitoriazzz@outlook.com>
2022-11-24 12:03:30 -0300

    classe IMC finalizada, retornando valor corretamente no terminal

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,13 +1,15 @@
 public class Calculadora {
 
   public double soma(double numeroA, double numeroB) {
-    return 0;
+    return numeroA + numeroB;
   }
   public double subtracao(double numeroA, double numeroB) {
-    return 0;
+    return numeroA - numeroB;
   }
-  public double divisao(double numeroA, double numeroB) {
-    return 0;
+  public static double divisao(double numeroA, double numeroB) {
+    return numeroA / numeroB;
   }
-
+  public static double multiplicacao(double numeroA, double numeroB) {
+    return numeroA * numeroB;
+  } 
 }
--- src/IMC.java
+++ src/IMC.java
@@ -1,12 +1,20 @@
-public class IMC {
-    
-    public IMC(Calculadora calculadora) {
-        this.calculadora = calculadora;
-    }
-    
-    Calculadora calculadora;
- 
-    public void calcularIMC(int peso, int altura) {
-        System.out.println(calculadora.divisao(peso, altura));
-    } 
+import java.util.Scanner;
+
+public class IMC {  
+    Scanner ler = new Scanner(System.in);  
+
+    public void calcularIMC() {  
+        int peso;
+        double altura;
+        
+        System.out.printf("Informe o peso: ");
+        peso = ler.nextInt();
+        System.out.printf("Informe a altura em metros: ");
+        altura = ler.nextDouble();
+
+        double calculo = Calculadora.multiplicacao(altura, altura);
+        double resultado = Calculadora.divisao(peso, calculo);
+
+        System.out.println("IMC: " + (Math. round(resultado)));
+    }    
 }  
