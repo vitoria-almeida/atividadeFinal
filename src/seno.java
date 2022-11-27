48d913ff9369916c8e83814873be27c435c73767
luanafernanda <lfsg.22@outlook.com>
2022-11-27 14:58:49 -0300

    troquei o nome metodo

--- src/seno.java
+++ src/seno.java
@@ -3,13 +3,12 @@ import java.util.Scanner;
 public class seno {
     Scanner scanner = new Scanner(System.in);  
 
-    public void seno() {  
+    public void resulSeno() {  
         
         double num;
       
         System.out.println("Digite um número para saber seu seno:  ");
         num = scanner.nextDouble();
-        System.out.println("O seno do número " + num +" é: " + Calculadora.seno(num));
+        System.out.println("O do número " + num +" é: " + Calculadora.seno(num));
     }
-
 }  
